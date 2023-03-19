0d5a239fb1ff01863acfba221d52a43503fb8117
RuiJCS <rjcsvv@gmail.com>
2023-03-19 19:50:52 +0000

    added Main scene

--- scenes/player.cs
+++ scenes/player.cs
@@ -5,12 +5,16 @@ public partial class player : Area2D
 {
 	[Export]
 	public int Speed = 400; // How fast the player will move (pixels/sec).
+		
+	[Signal]
+	public delegate void HitEventHandler();
 
 	public Vector2 ScreenSize; // Size of the game window.
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Hide();
 		ScreenSize = GetViewportRect().Size;
 	}
 
@@ -53,8 +57,23 @@ public partial class player : Area2D
 		}
 		Position += velocity * (float)delta;
 		Position = new Vector2(
-  		x: Mathf.Clamp(Position[0], 0, ScreenSize[0]),
-  		y: Mathf.Clamp(Position[1], 0, ScreenSize[1])
-);
+	  		x: Mathf.Clamp(Position[0], 0, ScreenSize[0]),
+	  		y: Mathf.Clamp(Position[1], 0, ScreenSize[1])
+		);
+	}
+
+	private void _on_body_entered(Node2D body)
+	{
+		Hide(); // Player disappears after being hit.
+		EmitSignal(SignalName.Hit);
+		// Must be deferred as we can't change physics properties on a physics callback.
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("disabled", true);
+	}
+
+	public void Start(Vector2 pos)
+	{
+		Position = pos;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
 	}
 }
